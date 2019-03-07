49dbb593674420aebeda6ba30a045b27417abf1e
calvinlcchen <42631868+calvinlcchen@users.noreply.github.com>
2019-03-07 14:15:39 +0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -125,7 +125,7 @@ function gotNewInputImg() {
   //        console.log("newImgUrl = "+newImgUrl + "uploader files = "+ uploader.files[0]+":"+uploader.files[1]);
   //   }, 2000);
      inputImg1.elt.src = newImgUrl
-     console.log("newImgUrl = "+newImgUrl.width+'x'+newImgUrl.height);
+     console.log("newImgUrl = "+inputImg1.elt.width+'x'+inputImg1.elt.height);
      inputImg.elt.src = newImgUrl;
      inputImg.elt.style.width='480px';
      inputImg.elt.style.height='480px';
